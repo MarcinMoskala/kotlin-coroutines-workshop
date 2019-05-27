c26976d0c2f9b80b8070ac4c122a473b393b6ed7
Marcin Moskala <marcinmoskala@gmail.com>
2019-05-27 10:06:22 +0200

    Add hint to Sieve

--- src/Sieve.kt
+++ src/Sieve.kt
@@ -5,6 +5,23 @@ val primes = sequence<Int> {
     TODO()
 }
 
+// TODO: Delete it and replace it with sequence builder (above)
+fun getPrimeNumbers(num: Int): List<Int> {
+    var numbers = generateSequence(2) { it + 1 }
+    val primes = mutableListOf<Int>()
+    repeat(num) {
+        val prime = numbers.first()
+        primes += prime
+        numbers = numbers.drop(1).filter { it % prime != 0 }
+    }
+    return primes
+}
+
+fun main() {
+    print(getPrimeNumbers(20))
+    // print(primes.take(20).toList())
+}
+
 @Suppress("FunctionName")
 class SieveTests {
 
