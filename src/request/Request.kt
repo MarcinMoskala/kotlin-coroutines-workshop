b3c09a6c456c8d53980ddff36036c79322a8f154
Marcin Moskala <marcinmoskala@gmail.com>
2019-09-02 17:01:11 +0200

    Improve request tests

--- src/request/Request.kt
+++ src/request/Request.kt
@@ -1,6 +1,10 @@
 package request
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.junit.jupiter.api.Test
 import kotlin.system.measureTimeMillis
 import kotlin.test.assertEquals
@@ -33,6 +37,16 @@ class ScopeTest {
     }
 
     @Test
+    fun `When no students, correct error is thrown`() = runBlocking {
+        val semester = "19L"
+        val best = Student(2, 95.0, semester)
+        val repo = ImmediateFakeStudentRepo(listOf())
+        assertThrowsError<NoSuchElementException> {
+            val chosen = getBestStudent(semester, repo)
+        }
+    }
+
+    @Test
     fun `Requests do not wait for each other`() = runBlocking {
         val repo = WaitingFakeStudentRepo()
         assertTimeAround(1200) {
@@ -50,6 +64,16 @@ class ScopeTest {
         job.cancel()
         delay(1000)
         assertEquals(0, repo.returnedStudents)
+    }
+
+    @Test
+    fun `When one request has error, all are stopped and error is thrown`() = runBlocking {
+        val repo = FirstFailingFakeStudentRepo()
+        assertThrowsError<FirstFailingFakeStudentRepo.FirstFailingError> {
+            getBestStudent("AAA", repo)
+        }
+        delay(1000)
+        assertEquals(0, repo.studentsReturned, "Looks like some requests were still running after the first one had an error")
     }
 }
 
@@ -72,6 +96,17 @@ inline fun assertTimeAround(expectedTime: Int, upperMargin: Int = 100, body: () 
     }
 }
 
+inline fun <reified T: Throwable> assertThrowsError(body: ()->Unit) {
+    try {
+        body()
+        assert(false) { "There should be an error of type ${T::class.simpleName}" }
+    } catch (throwable: Throwable) {
+        if(throwable !is T) {
+            throw throwable
+        }
+    }
+}
+
 class WaitingFakeStudentRepo : StudentsRepository {
     var returnedStudents = 0
 
@@ -85,4 +120,30 @@ class WaitingFakeStudentRepo : StudentsRepository {
         returnedStudents++
         return Student(12, 12.0, "AAA")
     }
+}
+
+class FirstFailingFakeStudentRepo : StudentsRepository {
+    var first = true
+    var studentsReturned = 0
+    val mutex = Mutex()
+
+    override suspend fun getStudentIds(semester: String): List<Int> {
+        delay(200)
+        return (1..5).toList()
+    }
+
+    override suspend fun getStudent(id: Int): Student {
+        delay(100)
+        mutex.withLock { // To prevent more than one throwing
+            if (first) {
+                first = false
+                throw FirstFailingError()
+            }
+        }
+        delay(100)
+        studentsReturned++
+        return Student(12, 12.0, "AAA")
+    }
+
+    class FirstFailingError(): Error()
 }
