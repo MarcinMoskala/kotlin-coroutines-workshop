35c11bbd0513598327c65c1398eb9b43d6ed478e
Marcin Moskala <marcin.moskala@allegro.pl>
2020-11-26 16:59:32 +0100

    Add Call.onResponse

--- src/github/GithubRepo.kt
+++ src/github/GithubRepo.kt
@@ -66,3 +66,15 @@ data class User(
         val login: String,
         val contributions: Int
 )
+
+inline fun <T> Call<T>.onResponse(crossinline callback: (Response<T>) -> Unit) {
+    enqueue(object : Callback<T> {
+        override fun onResponse(call: Call<T>, response: Response<T>) {
+            callback(response)
+        }
+
+        override fun onFailure(call: Call<T>, t: Throwable) {
+            throw t
+        }
+    })
+}
