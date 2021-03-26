10053b930759b87cde994552c3267114a816bdec
Marcin Moskala <marcin.moskala@allegro.pl>
2021-03-26 10:07:29 -0700

    Improve

--- src/github/Channels.kt
+++ src/github/Channels.kt
@@ -1,39 +1,36 @@
 package github
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
-fun main() = runBlocking(Dispatchers.Default) {
-    val username = "<Your Github username>"
-    // Link https://github.com/settings/tokens/new
-    // No permissions needed
-    val token = "<Github token>"
-    val service: GitHubService = createGitHubService(username, token)
+private const val username = "marcinmoskala"
+private const val token = "f79e01cd92d606a8369c4523d22a384ef4f16b71"
+private val service: GitHubService = createGitHubService(username, token)
 
-    val usersChannel = Channel<List<User>>()
-    launch {
-        getContributions(service, usersChannel)
-    }
+
+fun main(): Unit = runBlocking(Dispatchers.Default) {
+    val usersChannel = getContributionsChannel(service)
     for (contributions in usersChannel) {
         println(contributions)
     }
 
-//    val aggregatedUsersChannel = Channel<List<User>>()
-//    launch {
-//        getAggregatedContributionsChannel(service, aggregatedUsersChannel)
-//    }
-//    for (aggregatedContributions in aggregatedUsersChannel) {
-//        println(aggregatedContributions.sortedByDescending { it.contributions })
-//    }
+    val aggregatedUsersChannel = getAggregatedContributionsChannel(service)
+    for (aggregatedContributions in aggregatedUsersChannel) {
+        println(aggregatedContributions.sortedByDescending { it.contributions })
+    }
 }
 
-suspend fun getContributions(service: GitHubService, usersChannel: SendChannel<List<User>>) {
-    TODO()
+suspend fun CoroutineScope.getContributionsChannel(service: GitHubService): ReceiveChannel<List<User>> = produce {
+    // TODO
 }
 
-suspend fun getAggregatedContributionsChannel(service: GitHubService, usersChannel: SendChannel<List<User>>) {
-    TODO()
+suspend fun CoroutineScope.getAggregatedContributionsChannel(service: GitHubService): ReceiveChannel<List<User>> = produce {
+    // TODO
 }
