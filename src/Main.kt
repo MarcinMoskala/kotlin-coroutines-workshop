5d4fee7c4bee5201aeff041cc890a145a9293075
Marcin Moskala <marcin.moskala@allegro.pl>
2021-10-07 08:49:55 +0200

    Clear starting point

--- src/Main.kt
+++ src/Main.kt
@@ -1,28 +1,5 @@
-import kotlinx.coroutines.*
-import kotlin.coroutines.*
+import kotlinx.coroutines.coroutineScope
 
-//sampleStart
-suspend fun main(): Unit =
-    withContext(newSingleThreadContext("Name1")) {
-        var continuation: Continuation<Unit>? = null
+suspend fun main(): Unit = coroutineScope {
 
-        launch(newSingleThreadContext("Name2")) {
-            delay(1000)
-            continuation?.resume(Unit)
-        }
-
-        launch(Dispatchers.Unconfined) {
-            println(Thread.currentThread().name) // Name1
-
-            suspendCoroutine<Unit> { continuation = it }
-
-            println(Thread.currentThread().name) // Name2
-
-            delay(1000)
-
-            println(Thread.currentThread().name)
-            // kotlinx.coroutines.DefaultExecutor
-            // (used by delay)
-        }
-    }
-//sampleEnd
+}
