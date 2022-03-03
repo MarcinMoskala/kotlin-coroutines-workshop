c5859b11c36ea21ac157037659ea1302321de86b
marcinmoskala <marcinmoskala@gmail.com>
2022-03-03 15:40:23 +0100

    Better Tweet intro

--- src/Tweets.kt
+++ src/Tweets.kt
@@ -3,33 +3,37 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
-fun main() = runBlocking<Unit> {
+fun main() = runBlocking {
+    val tweets = async { getTweets() }
     val details = try {
         getUserDetails()
     } catch (e: Error) {
         null
     }
-    val tweets = async { getTweets() }
     println("User: $details")
     println("Tweets: ${tweets.await()}")
 }
 
-suspend fun CoroutineScope.getUserDetails(): Details {
-    val userName = async { getUserName() }
-    val followersNumber = async { getFollowersNumber() }
-    return Details(userName.await(), followersNumber.await())
+suspend fun getUserDetails(): Details {
+    val userName = getUserName()
+    val followersNumber = getFollowersNumber()
+    return Details(userName, followersNumber)
 }
 
 data class Details(val name: String, val followers: Int)
 data class Tweet(val text: String)
 
-fun getFollowersNumber(): Int = throw Error("Service exception")
+suspend fun getFollowersNumber(): Int {
+    delay(1000)
+    return 42
+}
 
 suspend fun getUserName(): String {
-    delay(500)
+    delay(1500)
     return "marcinmoskala"
 }
 
 suspend fun getTweets(): List<Tweet> {
+    delay(2000)
     return listOf(Tweet("Hello, world"))
 }
