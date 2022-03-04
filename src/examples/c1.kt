31b8d844429e3f466607d97886cb74c7b24d7ea2
marcinmoskala <marcinmoskala@gmail.com>
2022-03-04 11:23:44 +0100

    Better

--- src/examples/c1.kt
+++ src/examples/c1.kt
@@ -9,13 +9,13 @@ fun main(): Unit = runBlocking {
     val channel = Channel<Int>()
     launch {
         repeat(5) { index ->
-            channel.send(index * 2)
             println("Producing next one")
+            channel.send(index * 2)
+            delay(1000)
         }
     }
 
     repeat(5) {
-        delay(1000)
         val received = channel.receive()
         println(received)
     }
