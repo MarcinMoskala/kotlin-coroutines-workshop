b802e0f2835bb674e85597ed39b930221d9cf567
marcinmoskala <marcinmoskala@gmail.com>
2022-10-22 14:05:50 +0200

    Update examples

--- src/examples/f1.kt
+++ src/examples/f1.kt
@@ -1,23 +1,9 @@
 package examples
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking<Unit> {
-    println("Started producing")
-    val channel = flow {
-        println("Flow started")
-        for (i in 1..3) {
-            delay(100)
-            emit(i)
-        }
-    }
-
-    delay(150)
-    println("Calling flow...")
-    channel.collect { value -> println(value) }
-    println("Consuming again...")
-    channel.collect { value -> println(value) }
+    flowOf(1,2,3)
+        .collect { print(it) }
 }
